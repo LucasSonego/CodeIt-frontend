490902c26ec9dbec007c30d169352ebccb97b7e9
Lucas Sônego <lucassonego@ufpr.br>
2020-05-29 15:41:46 -0300

    :pencil2: refactor: Componentes separados para a página exibida para estudante e professor

--- src/pages/Disciplines/index.js
+++ src/pages/Disciplines/index.js
@@ -1,21 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
-import { store } from "react-notifications-component";
-import NotificationBody from "../../components/Notification";
-import { FiPlusCircle } from "react-icons/fi";
 
-import api from "../../services/api";
-
-import { Container } from "./styles";
 import getUserData from "../../util/getUserData";
-import Discipline from "./Discipline";
+import DisciplinesList from "./StudentPage";
+
+// import { Container } from './styles';
 
 function Disciplines() {
-  const [disciplines, setDisciplines] = useState([]);
-  const [enrolledDisciplines, setEnrolledDisciplines] = useState([]);
   const dispatch = useDispatch();
   const history = useHistory();
+
+  const [userData, setUserData] = useState({});
 
   useEffect(() => {
     dispatch({
@@ -23,140 +19,21 @@ function Disciplines() {
       page: "disciplinas",
     });
     async function awaitUserData() {
-      await getUserData({
-        dispatch,
-        history,
-        newtoken: true,
-      });
+      const respose = await getUserData({ dispatch, history, newtoken: true });
+      setUserData(respose);
     }
-
     awaitUserData();
-    const token = localStorage.getItem("token");
-    async function getDisciplines() {
-      const response = await api.get("/disciplines", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      setDisciplines(response.data.disciplines);
-      setEnrolledDisciplines(response.data.enrolled_disciplines);
-    }
-    getDisciplines();
   }, [dispatch, history]);
 
-  function sendNotification(content) {
-    store.addNotification({
-      content,
-      insert: "top",
-      container: "top-right",
-      animationIn: ["animated", "fadeIn"],
-      animationOut: ["animated", "fadeOut"],
-      dismiss: {
-        duration: 4000,
-        onScreen: false,
-      },
-    });
-  }
-
-  async function manageEnrollment(action, discipline, setLoadingState) {
-    setLoadingState(true);
-    const token = localStorage.getItem("token");
-
-    let response;
-
-    if (action === "create") {
-      response = await api.post(
-        `/enrollments/${discipline}`,
-        {},
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-    } else if (action === "remove") {
-      response = await api.delete(`/enrollments/${discipline}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-    }
-
-    if (response.status === 200) {
-      const content = (
-        <NotificationBody
-          type="success"
-          description={`Matrícula ${
-            action === "create" ? "efetuada" : "removida"
-          } com sucesso`}
-        />
-      );
-      sendNotification(content);
-    } else {
-      const content = (
-        <NotificationBody
-          type="error"
-          description={`Não foi possivel ${
-            action === "create" ? "efetuada" : "removida"
-          } sua matrícula`}
-        />
-      );
-      sendNotification(content);
-    }
-
-    response = await api.get("/disciplines", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    setLoadingState(false);
-    setDisciplines(response.data.disciplines);
-    setEnrolledDisciplines(response.data.enrolled_disciplines);
-    console.log(enrolledDisciplines.length);
-  }
-
   return (
-    <Container>
-      <div className="disciplines">
-        <div className="enrolleddisciplines">
-          <h3>Disciplinas que você está matriculado</h3>
-          <ul>
-            {enrolledDisciplines &&
-              enrolledDisciplines.map(discipline => (
-                <Discipline
-                  key={discipline.id}
-                  data={discipline}
-                  enrolled="true"
-                  buttonAction={manageEnrollment}
-                />
-              ))}
-            {enrolledDisciplines.length === 0 && (
-              <div className="noenrolleddisciplines">
-                <span>
-                  Você ainda não está matriculado em nenhuma disciplina
-                  <br />
-                  Para efetuar sua matricula em uma disciplina clique no
-                </span>
-                <FiPlusCircle />
-              </div>
-            )}
-          </ul>
-        </div>
-        <div className="otherdisciplines">
-          <h3>Outras disciplinas</h3>
-          <ul>
-            {disciplines &&
-              disciplines.map(discipline => (
-                <Discipline
-                  key={discipline.id}
-                  data={discipline}
-                  buttonAction={manageEnrollment}
-                />
-              ))}
-          </ul>
-        </div>
-      </div>
-    </Container>
+    <>
+      {userData.user &&
+        (userData.user.is_teacher ? (
+          <h3>teacher page</h3>
+        ) : (
+          <DisciplinesList />
+        ))}
+    </>
   );
 }
 
