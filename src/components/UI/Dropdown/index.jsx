6e0923a5ee1785844c7baa0dc1456126235d8b06
Lucas Sônego <lucassonego@ufpr.br>
2020-07-09 17:01:49 -0300

    :sparkles: feat: Componente Dropdown

---
+++ src/components/UI/Dropdown/index.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { MdExpandLess, MdExpandMore } from "react-icons/md";
+
+import { Container } from "./styles";
+
+function Dropdown({ items, value, onChange, disabled }) {
+  const [showList, setShowList] = useState(false);
+
+  return (
+    <Container disabled={disabled}>
+      <button
+        className="selected"
+        onClick={() => {
+          if (!disabled) {
+            showList ? setShowList(false) : setShowList(true);
+          }
+        }}
+      >
+        <span>{value.label}</span>
+        {!disabled && <>{showList ? <MdExpandLess /> : <MdExpandMore />}</>}
+      </button>
+
+      {showList && items && (
+        <>
+          <ul>
+            {items.map(item => (
+              <li
+                key={item.label}
+                onClick={() => {
+                  onChange(item);
+                  setShowList(false);
+                }}
+              >
+                <span>{item.label}</span>
+              </li>
+            ))}
+          </ul>
+          <div className="backdrop" onClick={() => setShowList(false)}></div>
+        </>
+      )}
+    </Container>
+  );
+}
+
+export default Dropdown;
