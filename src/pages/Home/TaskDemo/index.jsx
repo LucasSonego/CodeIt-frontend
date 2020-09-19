43680bcffdeda6b8b1d923c6a8f71fee568f9fa5
Lucas Sônego <lucassonego@ufpr.br>
2020-09-19 14:10:11 -0300

    :sparkles: feat: Section demonstrando a criação de tarefa (professor)

--- src/pages/Home/TaskDemo/index.jsx
+++ src/pages/Home/TaskDemo/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FiCode } from "react-icons/fi";
 
 import { Container } from "./styles";
+import teacherPage from "../../../assets/demos/task-teacher.svg";
 import studentPage from "../../../assets/demos/task-student.svg";
 
 function TaskDemo({ teacher }) {
@@ -11,7 +12,24 @@ function TaskDemo({ teacher }) {
         <FiCode /> <h2>Tarefa</h2>
       </div>
       {teacher ? (
-        <div className="teacher"></div>
+        <div className="teacher">
+          <img src={teacherPage} alt="" srcset="" />
+          <span className="tip">
+            Ao clicar em "Criar nova tarefa" o painel de criação de tarefa irá
+            se expandir;
+          </span>
+          <span className="tip">
+            As tarefas devem conter um título e um enunciado;
+          </span>
+          <span className="tip">
+            Você pode especificar uma linguagem para a tarefa ou optar por
+            aceitar respostas em qualquer linguagem;
+          </span>
+          <span className="tip">
+            (Opcional) É possível também enviar um trecho de código na tarefa
+            (mesmo que a tarefa aceite respostas em qualquer linguagem);
+          </span>
+        </div>
       ) : (
         <div className="student">
           <img src={studentPage} alt="" srcset="" />
